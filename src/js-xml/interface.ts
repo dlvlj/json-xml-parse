d4f569fe42d9f84724da8012ec44b1892c311e17
divij <divij.827@gmail.com>
2024-02-18 20:06:56 -0500

    refactoring

--- src/js-xml/interface.ts
+++ src/js-xml/interface.ts
@@ -6,8 +6,8 @@ export interface DeclarationData extends InputData {
   version?: "1.0" | "1.1";
   standalone?: "yes" | "no",
   encoding?: "UTF-8" | "UTF-16" | "US-ASCII" | "ISO-10646-UCS-2" | "ISO-10646-UCS-4"
-  | "ISO-8859-1" | "ISO-8859-2" | "ISO-8859-3" | "ISO-8859-4" | "ISO-8859-5" | "ISO-8859-6" | "ISO-8859-7" | "ISO-8859-8" | "ISO-8859-9"
-  | "ISO-2022-JP" | "Shift_JIS" | "EUC-JP" | string
+    | "ISO-8859-1" | "ISO-8859-2" | "ISO-8859-3" | "ISO-8859-4" | "ISO-8859-5" | "ISO-8859-6" | "ISO-8859-7" | "ISO-8859-8" | "ISO-8859-9"
+    | "ISO-2022-JP" | "Shift_JIS" | "EUC-JP" | string
 }
 
 export interface InputProps {
